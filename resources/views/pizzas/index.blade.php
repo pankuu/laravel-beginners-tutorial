25602b6eab89fc2deae54607f13563481d059356
marcinpank <marcin.pankiewicz@gmail.com>
2020-06-04 13:12:15 +0200

    Added sass to project

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            <div class="title m-b-md">
-                Pizza List
+    <div class="wrapper pizza-index">
+        <h1>
+            Pizza Orders
+        </h1>
+        <p class="mssg">{{ session('mssg') }}</p>
+        @foreach($pizzas as $pizza)
+            <div class="pizza-item">
+                <img src="/img/pizza.png" alt="pizza">
+                <h4><a href="/pizzas/{{ $pizza->id }}">{{ $pizza->name }}</a></h4>
             </div>
-            <p class="mssg">{{ session('mssg') }}</p>
-            @foreach($pizzas as $pizza)
-                <div>
-                    <a href="/pizzas/{{ $pizza->id }}">{{ $pizza->name }} - {{ $pizza->type }} - {{ $pizza->base }}</a>
-                </div>
-            @endforeach
-        </div>
+        @endforeach
     </div>
 @endsection
