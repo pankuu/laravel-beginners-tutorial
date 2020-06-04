d26126f9be6613465ece25bdf4a4c743ac49ec16
marcinpank <marcin.pankiewicz@gmail.com>
2020-06-04 14:20:34 +0200

    Added laravel/ui library, improvement views

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::post('/pizzas', 'PizzaController@store');
 Route::get('/pizzas/create', 'PizzaController@create');
 Route::get('/pizzas/{id}', 'PizzaController@show');
 Route::delete('/pizzas/{id}', 'PizzaController@destroy');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
